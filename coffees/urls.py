5d7d48ea48af6f1dcc19215318acaf0929697b93
Billy Huang <10646003@ntub.edu.tw>
2021-03-19 11:01:01 +0800

    feat(coffees): redesign index page

--- coffees/urls.py
+++ coffees/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'coffees'
 urlpatterns = [
     # ex: /coffees/
-    path('', views.index),
+    path('', views.index, name='index'),
 ]
--- coffees/views.py
+++ coffees/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Coffee
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>咖啡</h1>')
+    coffees = Coffee.objects.all()
+    return render(request, 'coffees/index.html', {'coffees': coffees})
